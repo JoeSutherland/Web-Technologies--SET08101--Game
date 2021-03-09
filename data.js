2723e208628e39d1e4bfbe6637015b50cd9f8ca6
JoeSutherland <78024920+JoeSutherland@users.noreply.github.com>
2021-03-09 09:51:48 +0000

    Added data.js and started work on User stats

---
+++ data.js
@@ -0,0 +1,30 @@
+TimePlayed = ['0'];
+TotalDeaths = ['0'];
+TotalEndings = ['0'];
+TotalGamesPlayed = ['0'];
+
+function deleteAllCookies() {
+  if (confirm('Are you sure you want to delete your savegame?')) {
+    valus = ['Timeplayed','TotalDeaths','TotalEndings','TotalGamesPlayed'];
+    for (i=0;i<valus.length;i++) {
+      if (getCookie(valus[i])) setCookie(valus[i],null,0);
+      if (localStorage.getItem(valus[i])) localStorage.removeItem(valus[i]);
+    }
+    setCookie('TimePlayed',null,0);
+    setCookie('TotalDeaths',null,0);
+    setCookie('TotalEndings',null,0);
+    setCookie('TotalGamesPlayed',null,0);
+    location.reload();
+  } else {
+  }
+}
+
+function ExportSave() { 
+
+  valus = ['TimePlayed','TotalDeaths','TotalEndings','TotalGamesPlayed'];
+  savedata = [];
+  for (i=0;i<valus.length;i++) {
+    j = getSave(valus[i]);
+    if (j) savedata.push(j);
+    else savedata.push('');
+  }
