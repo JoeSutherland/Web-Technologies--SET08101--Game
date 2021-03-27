62d14b0a3e2cb64e8509f10d609dfbd8811ba61f
JoeSutherland <78024920+JoeSutherland@users.noreply.github.com>
2021-03-27 14:05:43 +0000

    Work on localstorage started

--- data.js
+++ data.js
@@ -7,3 +7,22 @@ function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+//localStorage.setItem('key', 'value');
+//alert(localStorage.getItem('key'));
+
+//localStorage.setItem(‘key’, JSON.stringify({name: ‘value’}));
+//alert(JSON.parse(localStorage.getItem('key')).name);
+
+function store(game, status){
+    localStorage.setItem(game, status);
+}
+function check(game){
+    return localStorage.getItem(game);
+}
+
+window.onload = function() {
+    store("ending1", false);
+    store("ending2", false);
+    store("ending3", false);
+    store("ending4", false);
+}
